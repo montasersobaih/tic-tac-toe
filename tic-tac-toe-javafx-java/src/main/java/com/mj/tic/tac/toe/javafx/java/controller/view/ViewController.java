28fe0312a92157c13708ca819cdeb76d21335c95
montaser <montaser.jjs@gmail.com>
2023-01-25 02:28:04 +0300

    Enhance implementation

--- tic-tac-toe-javafx-java/src/main/java/com/mj/tic/tac/toe/javafx/java/controller/view/ViewController.java
+++ tic-tac-toe-javafx-java/src/main/java/com/mj/tic/tac/toe/javafx/java/controller/view/ViewController.java
@@ -4,16 +4,15 @@ import com.mj.tic.tac.toe.javafx.java.controller.BaseController;
 import com.mj.tic.tac.toe.javafx.java.controller.layout.ApplicationBarController;
 import com.mj.tic.tac.toe.javafx.java.dialog.ConfirmDialog;
 import com.mj.tic.tac.toe.javafx.java.task.CheckWinnerTask;
+import com.mj.tic.tac.toe.javafx.java.task.FinishGameTask;
 import com.mj.tic.tac.toe.javafx.java.task.MarkCellTask;
 import com.mj.tic.tac.toe.javafx.java.task.ResetPlayAreaTask;
-import com.mj.tic.tac.toe.javafx.java.util.Coordinates;
 import com.mj.tic.tac.toe.javafx.java.util.Mark;
 import com.mj.tic.tac.toe.javafx.java.util.Winner;
+import javafx.concurrent.Task;
 import javafx.concurrent.Worker;
 import javafx.concurrent.WorkerStateEvent;
-import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -22,7 +21,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
 import java.net.URL;
-import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
@@ -72,7 +70,7 @@ public final class ViewController extends BaseController {
     }
 
     private void resetPlayArea() {
-        ResetPlayAreaTask task = new ResetPlayAreaTask(matrix, playAreaPane);
+        Task<?> task = new ResetPlayAreaTask(matrix, playAreaPane);
         task.setOnSucceeded(this::onTaskSucceeded);
         executor.execute(task);
     }
@@ -97,7 +95,7 @@ public final class ViewController extends BaseController {
     private void onCellClicked(MouseEvent event) {
         Button button = (Button) event.getSource();
 
-        MarkCellTask task = new MarkCellTask(matrix, button, super.getTurn());
+        Task<?> task = new MarkCellTask(matrix, button, super.getTurn());
         task.setOnSucceeded(this::onTaskSucceeded);
         executor.execute(task);
     }
@@ -109,41 +107,30 @@ public final class ViewController extends BaseController {
         } else if (worker instanceof MarkCellTask) {
             Mark mark = (Mark) worker.getValue();
 
-            CheckWinnerTask checkWinnerTask = new CheckWinnerTask(matrix, mark);
-            checkWinnerTask.setOnSucceeded(this::onTaskSucceeded);
-            executor.execute(checkWinnerTask);
+            Task<?> task = new CheckWinnerTask(matrix, mark);
+            task.setOnSucceeded(this::onTaskSucceeded);
+            executor.execute(task);
         } else if (worker instanceof CheckWinnerTask) {
             Winner winner = (Winner) worker.getValue();
             if (Objects.nonNull(winner) || super.nextCount() == 9) {
                 Label label;
-                String message;
                 if (Objects.nonNull(winner)) {
                     label = super.getTurn() == 0 ? totalXWins : totalOWins;
-                    message = getString("message.alert.player.winner");
-                    message = String.format(message, winner.getPlayer());
-
-                    List<Node> buttons = playAreaPane.getChildren();
-                    for (Coordinates location : winner.getLocations()) {
-                        Button button = (Button) buttons.get(location.getX() * 3 + location.getY());
-                        button.pseudoClassStateChanged(PseudoClass.getPseudoClass("winner"), true);
-                    }
                 } else {
                     label = totalDraw;
-                    message = getString("message.alert.player.draw");
                 }
 
-                wins.getItems().add(message.split(", ")[0]);
-
-                int oldValue = Integer.parseInt(label.getText());
-                label.setText(String.valueOf(oldValue + 1));
-
-                ConfirmDialog.getInstance(contentPane)
-                        .setMessage(message)
-                        .setOnConfirmListener(this::resetPlayArea)
-                        .setOnDeclineListener(() -> playAreaPane.setDisable(true))
-                        .build()
-                        .show();
+                Task<?> task = new FinishGameTask(winner, playAreaPane, wins, label);
+                task.setOnSucceeded(this::onTaskSucceeded);
+                executor.execute(task);
             }
+        } else if (worker instanceof FinishGameTask) {
+            ConfirmDialog.getInstance(contentPane)
+                    .setMessage(worker.getMessage())
+                    .setOnConfirmListener(this::resetPlayArea)
+                    .setOnDeclineListener(() -> playAreaPane.setDisable(true))
+                    .build()
+                    .show();
         }
     }
 }
